0ab4046e9680701e8b300889e25ba36f547ae81d
Maximilian Witte <maximilian.witte@assurant.com>
2017-10-21 15:39:54 -0500

    Update schedule data file

--- public/javascripts/components/Schedule/ScheduleData.js
+++ public/javascripts/components/Schedule/ScheduleData.js
@@ -5,28 +5,28 @@ const data = [
     items: [
       {
         id: 1,
-        name: 'Cook, Serve, Delicious',
-        link: 'http://store.steampowered.com/app/247020/',
+        name: 'Starbound',
+        link: 'https://playstarbound.com/',
       },
       {
         id: 2,
-        name: 'Cook, Serve, Delicious',
-        link: 'http://store.steampowered.com/app/247020/',
+        name: 'Starbound',
+        link: 'https://playstarbound.com/',
       },
       {
         id: 3,
-        name: 'Cook, Serve, Delicious',
-        link: 'http://store.steampowered.com/app/247020/',
+        name: 'Starbound',
+        link: 'https://playstarbound.com/',
       },
       {
         id: 4,
-        name: 'Cook, Serve, Delicious',
-        link: 'http://store.steampowered.com/app/247020/',
+        name: 'Starbound',
+        link: 'https://playstarbound.com/',
       },
       {
         id: 5,
-        name: 'Cook, Serve, Delicious',
-        link: 'http://store.steampowered.com/app/247020/',
+        name: '',
+        link: '',
       },
     ],
   },
@@ -36,28 +36,741 @@ const data = [
     items: [
       {
         id: 1,
-        name: 'Cook, Serve, Delicious',
-        link: 'http://store.steampowered.com/app/247020/',
+        name: 'Starbound',
+        link: 'https://playstarbound.com/',
       },
       {
         id: 2,
-        name: 'Cook, Serve, Delicious',
-        link: 'http://store.steampowered.com/app/247020/',
+        name: 'Starbound',
+        link: 'https://playstarbound.com/',
       },
       {
         id: 3,
-        name: 'Cook, Serve, Delicious',
-        link: 'http://store.steampowered.com/app/247020/',
+        name: 'Starbound',
+        link: 'https://playstarbound.com/',
       },
       {
         id: 4,
-        name: 'Cook, Serve, Delicious',
-        link: 'http://store.steampowered.com/app/247020/',
+        name: 'Starbound',
+        link: 'https://playstarbound.com/',
       },
       {
         id: 5,
-        name: 'Cook, Serve, Delicious',
-        link: 'http://store.steampowered.com/app/247020/',
+        name: '',
+        link: '',
+      },
+    ],
+  },
+  {
+    id: 3,
+    time: '10:00am',
+    items: [
+      {
+        id: 1,
+        name: 'Warhammer: End Times - Vermintide',
+        link: 'http://store.steampowered.com/app/235540/Warhammer_End_Times__Vermintide/',
+      },
+      {
+        id: 2,
+        name: 'Warhammer: End Times - Vermintide',
+        link: 'http://store.steampowered.com/app/235540/Warhammer_End_Times__Vermintide/',
+      },
+      {
+        id: 3,
+        name: 'Warhammer: End Times - Vermintide',
+        link: 'http://store.steampowered.com/app/235540/Warhammer_End_Times__Vermintide/',
+      },
+      {
+        id: 4,
+        name: 'Warhammer: End Times - Vermintide',
+        link: 'http://store.steampowered.com/app/235540/Warhammer_End_Times__Vermintide/',
+      },
+      {
+        id: 5,
+        name: '',
+        link: '',
+      },
+    ],
+  },
+  {
+    id: 4,
+    time: '11:00am',
+    items: [
+      {
+        id: 1,
+        name: 'Warhammer: End Times - Vermintide',
+        link: 'http://store.steampowered.com/app/235540/Warhammer_End_Times__Vermintide/',
+      },
+      {
+        id: 2,
+        name: 'Warhammer: End Times - Vermintide',
+        link: 'http://store.steampowered.com/app/235540/Warhammer_End_Times__Vermintide/',
+      },
+      {
+        id: 3,
+        name: 'Warhammer: End Times - Vermintide',
+        link: 'http://store.steampowered.com/app/235540/Warhammer_End_Times__Vermintide/',
+      },
+      {
+        id: 4,
+        name: 'Warhammer: End Times - Vermintide',
+        link: 'http://store.steampowered.com/app/235540/Warhammer_End_Times__Vermintide/',
+      },
+      {
+        id: 5,
+        name: '',
+        link: '',
+      },
+    ],
+  },
+  {
+    id: 5,
+    time: '12:00pm',
+    items: [
+      {
+        id: 1,
+        name: 'Rocket League',
+        link: 'https://www.rocketleague.com/',
+      },
+      {
+        id: 2,
+        name: 'Dungeons of Dredmor',
+        link: 'https://dungeonsofdredmor.com/',
+      },
+      {
+        id: 3,
+        name: '',
+        link: '',
+      },
+      {
+        id: 4,
+        name: 'Horizon: Zero Dawn',
+        link: 'https://www.playstation.com/en-us/games/horizon-zero-dawn-ps4/',
+      },
+      {
+        id: 5,
+        name: '',
+        link: '',
+      },
+    ],
+  },
+  {
+    id: 6,
+    time: '1:00pm',
+    items: [
+      {
+        id: 1,
+        name: 'Rocket League',
+        link: 'https://www.rocketleague.com/',
+      },
+      {
+        id: 2,
+        name: 'FTL',
+        link: 'https://subsetgames.com/ftl.html',
+      },
+      {
+        id: 3,
+        name: '',
+        link: '',
+      },
+      {
+        id: 4,
+        name: '',
+        link: '',
+      },
+      {
+        id: 5,
+        name: '',
+        link: '',
+      },
+    ],
+  },
+  {
+    id: 7,
+    time: '2:00pm',
+    items: [
+      {
+        id: 1,
+        name: 'Tooth and Tail',
+        link: 'http://www.toothandtailgame.com/',
+      },
+      {
+        id: 2,
+        name: 'The Flame in the Flood',
+        link: 'http://www.themolassesflood.com/',
+      },
+      {
+        id: 3,
+        name: 'Tooth and Tail',
+        link: 'http://www.toothandtailgame.com/',
+      },
+      {
+        id: 4,
+        name: '',
+        link: '',
+      },
+      {
+        id: 5,
+        name: '',
+        link: '',
+      },
+    ],
+  },
+  {
+    id: 8,
+    time: '3:00pm',
+    items: [
+      {
+        id: 1,
+        name: 'Offworld Trading Company',
+        link: 'http://www.offworldgame.com/',
+      },
+      {
+        id: 2,
+        name: 'Offworld Trading Company',
+        link: 'http://www.offworldgame.com/',
+      },
+      {
+        id: 3,
+        name: 'Offworld Trading Company',
+        link: 'http://www.offworldgame.com/',
+      },
+      {
+        id: 4,
+        name: 'Overwatch',
+        link: 'https://playoverwatch.com/en-us/',
+      },
+      {
+        id: 5,
+        name: '',
+        link: '',
+      },
+    ],
+  },
+  {
+    id: 9,
+    time: '4:00pm',
+    items: [
+      {
+        id: 1,
+        name: 'Offworld Trading Company',
+        link: 'http://www.offworldgame.com/',
+      },
+      {
+        id: 2,
+        name: 'Offworld Trading Company',
+        link: 'http://www.offworldgame.com/',
+      },
+      {
+        id: 3,
+        name: 'Offworld Trading Company',
+        link: 'http://www.offworldgame.com/',
+      },
+      {
+        id: 4,
+        name: 'Overwatch',
+        link: 'https://playoverwatch.com/en-us/',
+      },
+      {
+        id: 5,
+        name: '',
+        link: '',
+      },
+    ],
+  },
+  {
+    id: 10,
+    time: '5:00pm',
+    items: [
+      {
+        id: 1,
+        name: 'Cogmind',
+        link: 'http://www.gridsagegames.com/cogmind/',
+      },
+      {
+        id: 2,
+        name: 'Invisible Inc.',
+        link: 'https://www.klei.com/games/invisible-inc',
+      },
+      {
+        id: 3,
+        name: 'FTL',
+        link: 'https://subsetgames.com/ftl.html',
+      },
+      {
+        id: 4,
+        name: '',
+        link: '',
+      },
+      {
+        id: 5,
+        name: '',
+        link: '',
+      },
+    ],
+  },
+  {
+    id: 11,
+    time: '6:00pm',
+    items: [
+      {
+        id: 1,
+        name: 'Cogmind',
+        link: 'http://www.gridsagegames.com/cogmind/',
+      },
+      {
+        id: 2,
+        name: 'Mark of the Ninja',
+        link: 'https://www.klei.com/games/mark-ninja',
+      },
+      {
+        id: 3,
+        name: 'FTL',
+        link: 'https://subsetgames.com/ftl.html',
+      },
+      {
+        id: 4,
+        name: '',
+        link: '',
+      },
+      {
+        id: 5,
+        name: '',
+        link: '',
+      },
+    ],
+  },
+  {
+    id: 12,
+    time: '7:00pm',
+    items: [
+      {
+        id: 1,
+        name: 'Killing Floor 2',
+        link: 'http://www.killingfloor2.com/',
+      },
+      {
+        id: 2,
+        name: 'Killing Floor 2',
+        link: 'http://www.killingfloor2.com/',
+      },
+      {
+        id: 3,
+        name: 'Killing Floor 2',
+        link: 'http://www.killingfloor2.com/',
+      },
+      {
+        id: 4,
+        name: 'Killing Floor 2',
+        link: 'http://www.killingfloor2.com/',
+      },
+      {
+        id: 5,
+        name: '',
+        link: '',
+      },
+    ],
+  },
+  {
+    id: 13,
+    time: '8:00pm',
+    items: [
+      {
+        id: 1,
+        name: 'Killing Floor 2',
+        link: 'http://www.killingfloor2.com/',
+      },
+      {
+        id: 2,
+        name: 'Killing Floor 2',
+        link: 'http://www.killingfloor2.com/',
+      },
+      {
+        id: 3,
+        name: 'Killing Floor 2',
+        link: 'http://www.killingfloor2.com/',
+      },
+      {
+        id: 4,
+        name: 'Killing Floor 2',
+        link: 'http://www.killingfloor2.com/',
+      },
+      {
+        id: 5,
+        name: '',
+        link: '',
+      },
+    ],
+  },
+  {
+    id: 14,
+    time: '9:00pm',
+    items: [
+      {
+        id: 1,
+        name: 'Company of Heroes 2',
+        link: 'http://www.companyofheroes.com/games/company-of-heroes-2',
+      },
+      {
+        id: 2,
+        name: 'The Long Dark',
+        link: 'http://www.thelongdark.com/',
+      },
+      {
+        id: 3,
+        name: 'Company of Heroes 2',
+        link: 'http://www.companyofheroes.com/games/company-of-heroes-2',
+      },
+      {
+        id: 4,
+        name: 'Armello',
+        link: 'http://armello.com/',
+      },
+      {
+        id: 5,
+        name: '',
+        link: '',
+      },
+    ],
+  },
+  {
+    id: 15,
+    time: '10:00pm',
+    items: [
+      {
+        id: 1,
+        name: 'Company of Heroes 2',
+        link: 'http://www.companyofheroes.com/games/company-of-heroes-2',
+      },
+      {
+        id: 2,
+        name: 'The Long Dark',
+        link: 'http://www.thelongdark.com/',
+      },
+      {
+        id: 3,
+        name: 'Company of Heroes 2',
+        link: 'http://www.companyofheroes.com/games/company-of-heroes-2',
+      },
+      {
+        id: 4,
+        name: 'Armello',
+        link: 'http://armello.com/',
+      },
+      {
+        id: 5,
+        name: '',
+        link: '',
+      },
+    ],
+  },
+  {
+    id: 16,
+    time: '11:00pm',
+    items: [
+      {
+        id: 1,
+        name: 'Cook Serve Delicious 2',
+        link: 'http://store.steampowered.com/app/386620/Cook_Serve_Delicious_2/',
+      },
+      {
+        id: 2,
+        name: 'The Long Dark',
+        link: 'http://www.thelongdark.com/',
+      },
+      {
+        id: 3,
+        name: '',
+        link: '',
+      },
+      {
+        id: 4,
+        name: 'Doom',
+        link: 'https://doom.com/en-us/',
+      },
+      {
+        id: 5,
+        name: '',
+        link: '',
+      },
+    ],
+  },
+  {
+    id: 17,
+    time: '12:00am',
+    items: [
+      {
+        id: 1,
+        name: 'Don‘t Starve',
+        link: 'https://www.klei.com/games/dont-starve-together',
+      },
+      {
+        id: 2,
+        name: 'Don‘t Starve',
+        link: 'https://www.klei.com/games/dont-starve-together',
+      },
+      {
+        id: 3,
+        name: '',
+        link: '',
+      },
+      {
+        id: 4,
+        name: 'Doom',
+        link: 'https://doom.com/en-us/',
+      },
+      {
+        id: 5,
+        name: 'Don‘t Starve',
+        link: 'https://www.klei.com/games/dont-starve-together',
+      },
+    ],
+  },
+  {
+    id: 18,
+    time: '1:00am',
+    items: [
+      {
+        id: 1,
+        name: 'Diablo 3',
+        link: 'https://us.battle.net/d3/en/',
+      },
+      {
+        id: 2,
+        name: 'Diablo 3',
+        link: 'https://us.battle.net/d3/en/',
+      },
+      {
+        id: 3,
+        name: '',
+        link: '',
+      },
+      {
+        id: 4,
+        name: 'Wolf Among Us',
+        link: 'https://telltale.com/series/the-wolf-among-us/',
+      },
+      {
+        id: 5,
+        name: 'Diablo 3',
+        link: 'https://us.battle.net/d3/en/',
+      },
+    ],
+  },
+  {
+    id: 19,
+    time: '1:00am',
+    items: [
+      {
+        id: 1,
+        name: 'Diablo 3',
+        link: 'https://us.battle.net/d3/en/',
+      },
+      {
+        id: 2,
+        name: 'Diablo 3',
+        link: 'https://us.battle.net/d3/en/',
+      },
+      {
+        id: 3,
+        name: '',
+        link: '',
+      },
+      {
+        id: 4,
+        name: 'Wolf Among Us',
+        link: 'https://telltale.com/series/the-wolf-among-us/',
+      },
+      {
+        id: 5,
+        name: 'Diablo 3',
+        link: 'https://us.battle.net/d3/en/',
+      },
+    ],
+  },
+  {
+    id: 20,
+    time: '2:00am',
+    items: [
+      {
+        id: 1,
+        name: 'The Witness',
+        link: '',
+      },
+      {
+        id: 2,
+        name: 'D&D: Chronicles of Mystara',
+        link: 'http://store.steampowered.com/app/229480/Dungeons__Dragons_Chronicles_of_Mystara/',
+      },
+      {
+        id: 3,
+        name: '',
+        link: '',
+      },
+      {
+        id: 4,
+        name: '',
+        link: '',
+      },
+      {
+        id: 5,
+        name: 'D&D: Chronicles of Mystara',
+        link: 'http://store.steampowered.com/app/229480/Dungeons__Dragons_Chronicles_of_Mystara/',
+      },
+    ],
+  },
+  {
+    id: 21,
+    time: '3:00am',
+    items: [
+      {
+        id: 1,
+        name: 'AI War',
+        link: 'http://arcengames.com/ai-war/',
+      },
+      {
+        id: 2,
+        name: 'Dungeon of the Endless',
+        link: 'http://store.steampowered.com/app/249050/Dungeon_of_the_Endless/',
+      },
+      {
+        id: 3,
+        name: '',
+        link: '',
+      },
+      {
+        id: 4,
+        name: '',
+        link: '',
+      },
+      {
+        id: 5,
+        name: '',
+        link: '',
+      },
+    ],
+  },
+  {
+    id: 22,
+    time: '4:00am',
+    items: [
+      {
+        id: 1,
+        name: 'AI War',
+        link: 'http://arcengames.com/ai-war/',
+      },
+      {
+        id: 2,
+        name: 'VVVVVV',
+        link: 'http://thelettervsixtim.es/',
+      },
+      {
+        id: 3,
+        name: '',
+        link: '',
+      },
+      {
+        id: 4,
+        name: 'Alan Wake',
+        link: 'http://www.alanwake.com/',
+      },
+      {
+        id: 5,
+        name: '',
+        link: '',
+      },
+    ],
+  },
+  {
+    id: 23,
+    time: '5:00am',
+    items: [
+      {
+        id: 1,
+        name: 'Kerbal Space Program',
+        link: 'https://kerbalspaceprogram.com/en/',
+      },
+      {
+        id: 2,
+        name: 'Legend of Grimrock',
+        link: 'http://www.grimrock.net/',
+      },
+      {
+        id: 3,
+        name: '',
+        link: '',
+      },
+      {
+        id: 4,
+        name: 'Alan Wake',
+        link: 'http://www.alanwake.com/',
+      },
+      {
+        id: 5,
+        name: '',
+        link: '',
+      },
+    ],
+  },
+  {
+    id: 24,
+    time: '6:00am',
+    items: [
+      {
+        id: 1,
+        name: 'Kerbal Space Program',
+        link: 'https://kerbalspaceprogram.com/en/',
+      },
+      {
+        id: 2,
+        name: 'Legend of Grimrock',
+        link: 'http://www.grimrock.net/',
+      },
+      {
+        id: 3,
+        name: '',
+        link: '',
+      },
+      {
+        id: 4,
+        name: '',
+        link: '',
+      },
+      {
+        id: 5,
+        name: '',
+        link: '',
+      },
+    ],
+  },
+  {
+    id: 25,
+    time: '7:00am',
+    items: [
+      {
+        id: 1,
+        name: 'Kerbal Space Program',
+        link: 'https://kerbalspaceprogram.com/en/',
+      },
+      {
+        id: 2,
+        name: 'Legend of Grimrock',
+        link: 'http://www.grimrock.net/',
+      },
+      {
+        id: 3,
+        name: '',
+        link: '',
+      },
+      {
+        id: 4,
+        name: '',
+        link: '',
+      },
+      {
+        id: 5,
+        name: '',
+        link: '',
       },
     ],
   },
