49c0017bd94cd86649df8efb3594b9656170f305
Ryan Breuer <breuerryan@gmail.com>
2018-10-21 21:21:46 -0500

    Update twitch urls

--- public/javascripts/components/User/UserData.js
+++ public/javascripts/components/User/UserData.js
@@ -2,32 +2,32 @@ const userdata = [
   {
     id: 'max',
     name: 'Maximilian',
-    twitch: 'http://twitch.tv/kinzata',
+    twitch: 'https://twitch.tv/kinzata',
   },
   {
     id: 'ryan',
     name: 'Ryan',
-    twitch: 'http://twitch.tv/ryan_plays_mediocrely',
+    twitch: 'https://twitch.tv/ryan_plays_mediocrely',
   },
   {
     id: 'andy',
     name: 'Andy',
-    twitch: 'http://twitch.tv/andsominity',
+    twitch: 'https://twitch.tv/andsominity',
   },
   {
     id: 'darrell',
     name: 'Darrell',
-    twitch: 'http://twitch.tv/dmfaber1',
+    twitch: 'https://twitch.tv/dmfaber1',
   },
   {
     id: 'eric',
     name: 'Eric',
-    twitch: 'http://twitch.tv/eric_plays_mediocrely',
+    twitch: 'https://twitch.tv/eric_plays_mediocrely',
   },
   {
     id: 'ian',
     name: 'Ian',
-    twitch: '',
+    twitch: 'https://twitch.tv/swissninja163',
   },
 ];
 
