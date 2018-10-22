329357bd84cbccd7c99c5033b45d04bb5b1a29b2
Ryan Breuer <breuerryan@gmail.com>
2018-10-21 22:26:09 -0500

    Fix page width overflow

--- public/javascripts/components/App.jsx
+++ public/javascripts/components/App.jsx
@@ -7,7 +7,7 @@ import '../../stylesheets/style.css';
 
 export default function App() {
   return (
-    <div className="row top-margin bottom-margin">
+    <div className="clearfix top-margin bottom-margin">
       <div className="col-xs-1" />
       <div className="col-xs-10 content border-shadow">
         <Header />
